fdbe2e1db8ad3df6fccb0d69212f9152085e12a9
LuisLopesDEV <LuisLopesDEV1@gmail.com>
2026-02-04 13:04:11 -0300

    Criaçao basica de cadastro
    Cadastro básico, ainda sem verificação de email ou obrigatóriedades de lógica, apenas hash de senha.

---
+++ DataBase/models.py
@@ -0,0 +1,60 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, Boolean, Float
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
+
+db = create_engine("mysql+pymysql://root:@localhost:3306/meubanco")
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column("id", Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column("name", String(50), nullable=False)
+    email = Column("email", String(50), unique=True, nullable=False)
+    senha = Column("senha", String(50), nullable=False)
+    ativo = Column("ativo", Boolean, nullable=False)
+    admin = Column("admin", Boolean, default=False)
+
+    def __init__(self, name, email, senha, ativo=True, admin=False):
+        self.name = name
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
+
+class Pedido(Base):
+    __tablename__ = "pedidos"
+
+    STATUS_PEDIDOS = (
+        ("PENDENTE", "PENDENTE"),
+        ("CANCELADO", "CANCELADO"),
+        ("FINALIZADO", "FINALIZADO")
+    )
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True, nullable=False)
+    status = Column("status", String(50), nullable=False) # Pendente, Cancelado, Finalizado
+    usuario = Column("usuario", ForeignKey('users.id'), nullable=False)
+    preco = Column("preco", Float, nullable=False)
+    #itens =
+
+    def __init__(self,usuario, status="PENDENTE", preco=0):
+        self.status = status
+        self.usuario = usuario
+        self.preco = preco
+
+class ItemPedido(Base):
+    __tablename__ = "itens_pedidos"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantidade = Column("quantidade", Integer, nullable=False)
+    sabor = Column("sabor", String(50), nullable=False)
+    tamanho = Column("tamanho", String(50), nullable=False)
+    preco_unitario = Column("preco_unitario", Float, nullable=False)
+    pedido = Column("pedido", ForeignKey('pedidos.id'), nullable=False)
+
+    def __init__ (self, sabor, pedido, tamanho, quantidade=1, preco_unitario=0):
+        self.sabor = sabor
+        self.pedido = pedido
+        self.tamanho = tamanho
+        seld.quantidade = quantidade
+        self.preco_unitario = preco_unitario
--- Routes/auth_routes.py
+++ Routes/auth_routes.py
@@ -1,7 +1,26 @@
 from fastapi import APIRouter
-
+from DataBase.models import User, db
+from sqlalchemy.orm import sessionmaker
+import bcrypt
 auth_router = APIRouter(prefix="/auth", tags=['Auth'])
 
 @auth_router.get('/')
-async def auth():
+async def home():
     return {'message': 'Path Auth'}
+
+@auth_router.post('/signup')
+async def signup(nome: str, email:str, senha: str, session):
+    senha_bytes = senha.encode('utf-8')
+    hash = bcrypt.hashpw(senha_bytes, bcrypt.gensalt())
+    hash_senha = hash.decode('utf-8')
+    sessions = sessionmaker(bind=db)
+    session = sessions()
+    usuario = session.query(User).filter(User.email == email).first()
+
+    if usuario:
+        return {'mensagem': 'Usuário já existe'}
+    else:
+        novo_usuario = User(nome, email, hash_senha)
+        session.add(novo_usuario)
+        session.commit()
+        return {'mensagem': 'Usuário criado com sucesso!'}
